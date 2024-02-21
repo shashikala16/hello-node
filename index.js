49f7a47f7a4480b7e889baa1c1c23c77dfefcd9c
shashikala16 <116083426+shashikala16@users.noreply.github.com>
2024-02-21 21:08:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-function hello(){
-    console.log("Hello Node.js!");
-
-}
-
-hello();
+const app = require("./app");
+app.listen(3000, () => {
+  console.log("started express server at 3000");
+});
+// "test": "NODE_ENV=test jest --detectOpenHandles"
